e1842e80ff03edc9b7e0c4aa275bb60cd65368bd
AlminHalilovic <almin.halilovic@hotmail.com>
2023-11-04 17:25:55 +0100

    feature/validation

--- src/main/java/com/example/bookings/domain/request/SignupRequest.java
+++ src/main/java/com/example/bookings/domain/request/SignupRequest.java
@@ -1,4 +1,10 @@
 package com.example.bookings.domain.request;
 
-public record SignupRequest(String firstName, String lastName, String email, String password) {
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotEmpty;
+
+public record SignupRequest(@NotEmpty String firstName,
+                            @NotEmpty String lastName,
+                            @NotEmpty @Email String email,
+                            @NotEmpty String password) {
 }
--- src/main/java/com/example/bookings/domain/request/UpdateBlockRequest.java
+++ src/main/java/com/example/bookings/domain/request/UpdateBlockRequest.java
@@ -1,6 +1,20 @@
 package com.example.bookings.domain.request;
 
+import com.example.bookings.utils.validators.StartDateEndDateConstraint;
+import jakarta.validation.constraints.FutureOrPresent;
+
 import java.time.LocalDate;
 
-public record UpdateBlockRequest(LocalDate startDate, LocalDate endDate) {
+@StartDateEndDateConstraint
+public record UpdateBlockRequest(@FutureOrPresent LocalDate startDate,
+                                 @FutureOrPresent LocalDate endDate) implements DateRangeRecord {
+    @Override
+    public LocalDate getStartDate() {
+        return startDate;
+    }
+
+    @Override
+    public LocalDate getEndDate() {
+        return endDate;
+    }
 }
